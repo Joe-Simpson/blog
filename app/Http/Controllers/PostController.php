e5e889744c0f0b81d3457b8a2f3243b6df38cd06
Joe-Simpson <the.fluff.lives.on@gmail.com>
2018-06-11 16:04:27 +0100

    added create post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
-    public function ()
+    public function index()
     {
     	return view('posts.index');
+    }
+
+    public function create()
+    {
+    	return view('posts.create');
+    }
+
+    public function store()
+    {
+    	// dd(request()->all());
+    	// create a new post
+    	$post = new Post;
+
+    	$post->title = request('title');
+    	$post->body = request('body');
+
+    	// save to the database
+
+    	$post->save();
+
+    	// return to homepage
+
+    	return redirect('/');
     }
 }
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="col-md-8 blog-main">
+  <h3 class="pb-3 mb-4 font-italic border-bottom">
+    Create
+  </h3>
+
+  <form method="POST" action="/posts">
+
+    {{ csrf_field() }}
+    
+    <div class="form-group">
+      <label for="title">Title:</label> 
+      <input type="text" class="form-control" id="title" name="title">
+    </div>
+
+    <div class="form-group">
+      <label for="title">Body:</label> 
+      <textarea id="body" name="body" class="form-control"></textarea>
+    </div>
+    
+    <button type="submit" class="btn btn-primary">Publish</button>
+
+  </form>
+
+</div>
+@endsection
