18d54201ab18d7feb3eefd3f5259d9fe7f90a381
Joe-Simpson <the.fluff.lives.on@gmail.com>
2018-06-13 16:37:05 +0100

    attempted fix for password encryption

--- app/User.php
+++ app/User.php
@@ -27,6 +27,14 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    /**
+     * Add a mutator to ensure hashed passwords
+     */
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
     public function posts()
     {
         $this->hasMany(Post::class);
